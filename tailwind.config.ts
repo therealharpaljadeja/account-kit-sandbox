2401517d02a50eb4b1bdb0a4aed4ab47b9020513
Harpal Jadeja <jadejaharpal14@gmail.com>
2024-01-30 01:24:56 +0530

    feat: add brand colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: (theme) => ({
+                "brand-gradient":
+                    "linear-gradient(to bottom right, #FF904A, #FE56B9)",
+            }),
+            colors: {
+                brand1: "#FF904A",
+                brand2: "#FE56B9",
+                "brand2-100": "#ffbbe3",
+            },
+            fontFamily: {
+                PlayFair: ["PlayFair Display"],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
