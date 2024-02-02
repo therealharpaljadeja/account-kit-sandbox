e55cc82ec5cfbda92c08d680ca1222be3cbbc86c
Harpal Jadeja <jadejaharpal14@gmail.com>
2024-02-03 00:18:36 +0530

    feat: open graph info

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,16 @@ export default function Document() {
         <Html lang="en">
             <Head>
                 <link rel="shortcut icon" href="/logos/tx-logo.png" />
+                <meta property="og:image" content="/ogimage.png" />
+                <meta property="og:title" content="Account Kit Sandbox" />
+                <meta
+                    property="og:description"
+                    content="A tool is to easily experience smart accounts built using Account Kit!"
+                />
+                <meta
+                    property="og:url"
+                    content="https://account-kit-sandbox.vercel.app/"
+                />
             </Head>
             <body>
                 <Main />
